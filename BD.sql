a10a8ff0238c01e16631a738f1f0ff7fb82346f9
IDGS-902-20002145 <20002145@alumnos.utleon.edu.mx>
2023-11-13 22:52:30 -0600

    Commit SQL
    Agregación de archivos SQL

---
+++ BD.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE PWA;
+
+USE PWA;
+
+CREATE TABLE BAZAR.PRODUCTO (
+    ProductID INT PRIMARY KEY,
+    Title NVARCHAR(255),
+    Description NVARCHAR(MAX),
+    Price DECIMAL(10, 2),
+    DiscountPercentage DECIMAL(5, 2),
+    Rating DECIMAL(3, 2),
+    Stock INT,
+    Brand NVARCHAR(100),
+    Category NVARCHAR(100),
+    ThumbnailUrl NVARCHAR(MAX)
+);
+
+CREATE TABLE BAZAR.IMAGEN_PRODUCTO (
+    ImageID INT PRIMARY KEY,
+    ProductID INT,
+    ImageUrl NVARCHAR(MAX),
+    FOREIGN KEY (ProductID) REFERENCES BAZAR.PRODUCTO (ProductID)
+);
